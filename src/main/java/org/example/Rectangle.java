fd075dd760d5eeb9b3d7fe4d50a2494c69281069
chryswds <dybas.chrys@gmail.com>
2024-10-29 10:02:53 +0000

    Rectangle class exceptions

--- src/main/java/org/example/Rectangle.java
+++ src/main/java/org/example/Rectangle.java
@@ -1,6 +1,23 @@
 package org.example;
 
 public class Rectangle {
+    int length;
+    int width;
+
+
+//    The constructor will manage/validate the input value
+
+    public Rectangle(int length, int width){
+//        Check for the values and check if we need to throw any exceptions
+
+        if( length <= 0 || width <= 0){
+            throw new NegativeNumberException("The sides must be positive");
+        }
+
+//        Otherwise if the value is bigger than zero
+        this.length = length;
+        this.width = width;
+    }
 
 
 }
