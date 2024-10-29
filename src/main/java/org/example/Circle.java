3e94b7a95a4966c5f52d7c6c2513254a2df02918
chryswds <dybas.chrys@gmail.com>
2024-10-29 09:58:05 +0000

    Circle class throwing exceptions

--- src/main/java/org/example/Circle.java
+++ src/main/java/org/example/Circle.java
@@ -1,4 +1,31 @@
 package org.example;
 
 public class Circle {
+
+//    The circle must have a radius
+//    We need to validate the inputs to be within our
+//    Specific radius value
+//    The minimum value should be bigger than 0
+
+//    Construct the attributes
+
+    int radius;
+
+//    Constructor to assign or retrieve the value
+
+    public Circle(int radius) throws ZeroLengthException {
+//        The condition to test four our exception
+        if(radius == 0){
+//            if it's 0 throw an exception message
+            throw new ZeroLengthException();
+        }else if( radius <  0){
+            throw new NegativeNumberException("Radius cannot be negative");
+        }else {
+//            Assign the radius value
+            this.radius = radius;
+        }
+    }
+
+
+
 }
